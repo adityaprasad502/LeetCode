4a4e6d6aafebaa3f34ca23342c0ee11b67ab7262
Aditya Prasad S <70315903+adityaprasad502@users.noreply.github.com>
2025-09-01 18:44:57 +0530

    Time: 3 ms (31.43%), Space: 17.8 MB (61.86%) - LeetHub

---
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canAliceWin(self, nums: List[int]) -> bool:
+        a, b = 0,0
+        for n in nums:
+            if n < 10:
+                a+=n
+            else:
+                b+=n
+        return a!=b
